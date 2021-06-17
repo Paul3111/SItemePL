f8a97d07f5e2eef0db1a1bbe84e368fa65dad04e
Paul3111 <82970627+Paul3111@users.noreply.github.com>
2021-06-17 23:55:38 +0100

    final

--- tictactoe_functions.py
+++ tictactoe_functions.py
@@ -1,22 +1,30 @@
 
 
-def empty_board():  # prints the empty board when the games starts
+def empty_board() -> bool:  # prints the empty board when the games starts
     print("\n\n")
     print("--|--|--")
     print("--|--|--")
     print("--|--|--")
     print("\n\n\n\n")
+    return True
 
 
-def print_board_after_move(board):  #this function updates the board after each move
+def print_board_after_move(board) -> bool:  # this function updates the board after each move
     print("\n")
     print(f"{board[1]} | {board[2]} | {board[3]}")
     print(f"{board[4]} | {board[5]} | {board[6]}")
     print(f"{board[7]} | {board[8]} | {board[9]}")
     print("\n")
+    return True
 
 
-def find_winner(list1, list2):  # this function checks if the winning combination is found in the player's moves list
+def find_winner(list1, list2) -> bool:
+    """
+    this function checks if the winning combination is found in the player's moves list
+    @param list1:
+    @param list2:
+    @return:
+    """
     if len(list(set(list1) & set(list2))) == 3:
         return True
     else:
