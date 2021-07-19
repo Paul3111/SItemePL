2ee1bd3b1e1656349f01f9a8f0002f097f55c2f6
Paul3111 <82970627+Paul3111@users.noreply.github.com>
2021-07-19 16:50:06 +0100

    final update

--- tema_scraping.py
+++ tema_scraping.py
@@ -1,14 +1,72 @@
 import requests
 from bs4 import BeautifulSoup
+# import xlsxwriter
 
-for covid_days in range(14, 21):
+
+############################# VERSION 1 - Main #################################
+
+# for covid_days in range(14, 21):
+#     covid_link = (f"https://www.mai.gov.ro/informare-covid-19-grupul-de"
+#                   f"-comunicare-strategica-{covid_days}-ianuarie-ora-13-00/")
+#
+#     r = requests.get(covid_link)
+#     s1 = BeautifulSoup(r.text, "html.parser")
+#
+#     file = open(f"covid_data_{covid_days}.csv", "w", encoding="utf-8")
+#
+#     for tr in s1.table.select("tr"):
+#         for value in tr.select("td"):
+#             if "," in value.text:
+#                 a = value.text.replace(",", ".")
+#                 file.write(f"{a},")
+#             else:
+#                 file.write(f"{value.text},")
+#         file.write("\n")
+
+
+############################# VERSION 2 - Functions (access any link) #################################
+
+# def get_covid_data(day_covid: int, month_covid: str) -> str:
+#     covid_link = (f"https://www.mai.gov.ro/informare-covid-19-grupul-de"
+#                   f"-comunicare-strategica-{day_covid}-{month_covid.lower ()}-ora-13-00/")
+#     # print(covid_link)
+#     r = requests.get (covid_link)
+#     s1 = BeautifulSoup (r.text, "html.parser")
+#
+#     file = open (f"covid_data_{month_covid}.{day_covid}.csv", "w", encoding="utf-8")
+#
+#     for tr in s1.table.select("tr"):
+#         for value in tr.select ("td"):
+#             if "," in value.text:
+#                 a = value.text.replace (",", ".")
+#                 file.write(f"{a},")
+#             else:
+#                 file.write (f"{value.text},")
+#         file.write ("\n")
+#     return f"Report generated for {day_covid}-{month_covid}!"
+#
+#
+# get_covid_data(15, "februarie")
+
+
+######################## VERSION 3 - Multiple dates on one file #################################
+
+start_date = 14
+end_date = 21
+month = "ianuarie"
+
+for covid_days in range(start_date, end_date):
     covid_link = (f"https://www.mai.gov.ro/informare-covid-19-grupul-de"
-                  f"-comunicare-strategica-{covid_days}-ianuarie-ora-13-00/")
+                  f"-comunicare-strategica-{covid_days}-{month}-ora-13-00/")
 
     r = requests.get(covid_link)
     s1 = BeautifulSoup(r.text, "html.parser")
 
-    file = open(f"covid_data_{covid_days}.csv", "w", encoding="utf-8")
+    file = open(f"covid_data_multiple_days.csv", "a+", encoding="utf-8")
+
+    file.write("\n")
+    file.write(f"Valori pentru {covid_days} {month}")
+    file.write("\n")
 
     for tr in s1.table.select("tr"):
         for value in tr.select("td"):
@@ -18,3 +76,27 @@ for covid_days in range(14, 21):
             else:
                 file.write(f"{value.text},")
         file.write("\n")
+
+
+############################# VERSION 4 (NOT WORKING) #################################
+
+# for covid_days in range(14, 21):
+#     covid_link = (f"https://www.mai.gov.ro/informare-covid-19-grupul-de"
+#                   f"-comunicare-strategica-{covid_days}-ianuarie-ora-13-00/")
+#
+#     r = requests.get(covid_link)
+#     s1 = BeautifulSoup(r.text, "html.parser")
+#
+#     workbook = xlsxwriter.Workbook(f"covid_data_{covid_days}.xls")
+#     worksheet = workbook.add_worksheet()
+#
+#     for tr in s1.table.select("tr"):
+#         for value in tr.select("td"):
+#             if "," in value.text:
+#                 a = value.text.replace(",", ".")
+#                 worksheet.write(str(a))
+#             else:
+#                 worksheet.write(str(value))
+#         worksheet.write("\n")
+
+
